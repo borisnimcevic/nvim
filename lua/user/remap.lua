98510709ac8ff765e6a39daf05b1f0d406d5876c
Boris Nimcevic <borisnimcevic@gmail.com>
2022-08-02 13:34:59 +0200

    Added easier window navigation

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,3 +1,19 @@
-local nnoremap = require("user.keymap").nnoremap
+local Remap = require("user.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
 
+-- open netrw
 nnoremap("<leader>pv","<cmd>Ex<CR>")
+
+-- easier window navigation
+nnoremap("<C-h>", "<C-w>h")
+nnoremap("<C-j>", "<C-w>j")
+nnoremap("<C-k>", "<C-w>k")
+nnoremap("<C-l>", "<C-w>l")
+
+-- Unmap
+nnoremap("J","<Nop>")
+nnoremap("<Space>", "<Nop>")
