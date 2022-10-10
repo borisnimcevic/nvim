932dd5fb844f66b5e3aa1a4d9a1bebd82215421a
Boris Nimcevic <borisnimcevic@gmail.com>
2022-10-10 23:41:55 +0200

    Update colors.

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,5 +3,5 @@ vim.g.tokyonight_transparent = true
 
 vim.opt.background = "dark"
 
--- vim.cmd("colorscheme tokyonight")
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme tokyonight")
+-- vim.cmd("colorscheme gruvbox")
