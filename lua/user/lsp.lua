6424184bcab1459a5fe4361701cf702eddfca5f9
Boris Nimcevic <borisnimcevic@gmail.com>
2022-11-15 15:58:13 +0100

    fixed formatting funciton

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -11,7 +11,7 @@ require('lspconfig').rust_analyzer.setup {
     vim.keymap.set("n","<leader>df",vim.diagnostic.goto_next, {buffer = 0})
     vim.keymap.set("n","<leader>dp",vim.diagnostic.goto_prev, {buffer = 0})
     vim.keymap.set("n","<leader>rn",vim.lsp.buf.rename, {buffer = 0})
-    vim.keymap.set("n","<leader>f",vim.lsp.buf.formatting, {buffer = 0})
+    vim.keymap.set("n","<leader>f",vim.lsp.buf.format, {buffer = 0})
   end,
 }
 
