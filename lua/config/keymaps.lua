dc0540643439aeb38efbdd6a4e401f2c89d59ff3
Boris Nimcevic <boris.nimcevic@quandify.com>
2025-01-14 10:51:18 +0100

    keymap: jump to corresponging header

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<Space>h", ":ClangdSwitchSourceHeader<CR>", { noremap = true, silent = true })
